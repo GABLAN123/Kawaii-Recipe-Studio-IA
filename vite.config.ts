3806ce38d08f93b854648fa0cda134585a79849e
GABLAN123 <108202136+GABLAN123@users.noreply.github.com>
2026-02-14 18:21:57 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Asegura que los archivos se encuentren en rutas relativas (importante para GitHub Pages)
+  base: './',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
+    emptyOutDir: true
   }
-});
-
 });
